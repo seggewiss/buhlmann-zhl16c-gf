852e7732c5142976b800b2aa41446ac2e4dd7748
Sebastian Seggewiss <s.seggewiss@shopware.com>
2021-12-21 21:24:29 +0100

    WIP: Reactive store

---
+++ src/components/Display.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="display">
+    {{ state.time }}s
+    {{ state.depth }}m
+
+    <button @click="startDive">Start</button>
+    <button @click="endDive">Stop</button>
+    <input type="number" min="0.1" step="0.1" v-model="distance"/>
+    <button @click="ascendByDistance">Ascend</button>
+    <button @click="descendByDistance">Descend</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue } from 'vue-class-component'
+import state, { start, stop, ascend, descend } from '../store'
+
+export default class Display extends Vue {
+  distance = 1
+  state = state
+
+  startDive (): void {
+    start()
+  }
+
+  endDive (): void {
+    stop()
+  }
+
+  ascendByDistance (): void {
+    ascend(this.distance)
+  }
+
+  descendByDistance (): void {
+    descend(this.distance)
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.display {
+  display: flex;
+}
+</style>
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,32 @@
-import { createStore } from 'vuex'
+import { reactive } from 'vue'
 
-export default createStore({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+const state = reactive({
+  time: 0,
+  depth: 0
 })
+
+let interval = 0
+
+export default state
+
+export function start (): void {
+  interval = setInterval(() => {
+    state.time += 1
+  }, 1000)
+}
+
+export function stop (): void {
+  if (interval === 0) {
+    return
+  }
+
+  clearInterval(interval)
+}
+
+export function ascend (distance: number): void {
+  state.depth += distance
+}
+
+export function descend (distance: number): void {
+  state.depth -= distance
+}
